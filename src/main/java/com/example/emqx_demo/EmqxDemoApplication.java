bb7dbb9b51570ebb2f2ccca3d0b409be599b7f0b
liushouhang <liushouhang@hhkj17.wecom.work>
2024-07-31 10:55:00 +0800

    111

---
+++ src/main/java/com/example/emqx_demo/EmqxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.emqx_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmqxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmqxDemoApplication.class, args);
+    }
+
+}
